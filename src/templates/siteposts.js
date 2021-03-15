2b1c44218383874a5c7d5e155e1c08ed06cbe76d
Adrian Felder <adrianfelder@gmx.ch>
2021-03-15 15:15:02 +0100

    added contentful richtext field formatting

--- src/templates/siteposts.js
+++ src/templates/siteposts.js
@@ -1,10 +1,28 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
+
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+
+
 const SitePosts = ({ data }) => {
-  const { heading, subheading, description, image } = data.contentfulProductBucher
+  const { heading, subheading, image } = data.contentfulProductBucher
+
+  const RichContent = JSON.parse(data.contentfulProductBucher.description.raw);
+
+  const options = {
+        renderNode: {
+            [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
+            // [BLOCKS.EMBEDDED_ASSET]:  <img src={`https:${data.contentfulProject.projectContent.references.fluid.src}`}/>,
+        },
+        renderMark: {},
+        }
+
   return (
     <Layout>
       <SEO title={heading} />
@@ -12,7 +30,7 @@ const SitePosts = ({ data }) => {
         <h1>{heading}</h1>
         {image ? <img alt={heading} src={image.file.url} /> : <div>bild</div> }
         <h3>{subheading}</h3>
-        <p>{description.raw}</p>
+        {documentToReactComponents(RichContent, options)}
 
         <Link to="/siteposts">View more posts</Link>
         <Link to="/">Back to Home</Link>
